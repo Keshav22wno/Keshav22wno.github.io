80d9e0c9a54e6e9538244ac528d1597d58772e75
Keshav22wno <keshav22wno@gmail.com>
2023-10-24 19:47:56 +0530

    First commit

---
+++ hello_page.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+def main():
+    main_v1()
+
+def main_v1():
+    print("we designed first main function)
+    name = input("Enter your name: ")
+    last = input("Enter your last name: ")
+    print("Your name is:",name,end=" ")
+    print("Your surname is:",last)
+    print("Say, Hello")
+    print("Say, Hi")
+    print("Say, Hi form GUI Git")
+    house = input("Enter your house:")
+    print("House: ",house)
+
+if __name__ == "__main__":
+    main()
+
